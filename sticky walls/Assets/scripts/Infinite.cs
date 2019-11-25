f65b75bc06f59522976aaf841e0687a382e679fe
DonJuan1232 <57268409+DonJuan1232@users.noreply.github.com>
2019-11-25 16:53:47 +0100

    Added project

---
+++ sticky walls/Assets/scripts/Infinite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infinite : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject wall1;
+    public GameObject wall2;
+
+    public Vector3 playerPos;
+
+    void Update()
+    {
+
+        playerPos = player.transform.position;
+
+        if (playerPos.y == 15f)
+        {
+            Instantiate(wall1, new Vector3(-8.03f, 35, 18.68f), Quaternion.identity);
+
+            Instantiate(wall2, new Vector3(8.03f, 35, 18.68f), Quaternion.identity);
+        }
+    }
+}
---
+++ sticky walls/Assets/scripts/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    bool WallTouchL = false;
+    bool WallTouchR = false;
+
+
+    public Rigidbody rb;
+
+    public GameObject wallL;
+    public GameObject wallR;
+    public GameObject player;
+
+    public void right()
+    {
+        if (WallTouchR == true)
+        {
+            rb.AddForce(-1500, 1000, 0);
+            new WaitForSeconds(0.5f);
+            WallTouchR = false;
+        }
+    }
+
+    public void left()
+    {
+        if (WallTouchL == true)
+        {
+            rb.AddForce(1500, 1000, 0);
+            new WaitForSeconds(0.5f);
+            WallTouchL = false;
+        }
+    }
+
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.transform.gameObject.name == "wallR" || hit.transform.gameObject.name == "wallR(Clone)")
+        {
+            WallTouchR = true;
+            
+        }
+
+        if (hit.transform.gameObject.name == "wallL" || hit.transform.gameObject.name == "wallL(Clone)")
+        {
+            WallTouchL = true;
+            
+        }
+    }
+
+    
+
+
+
+}
