9b1e7b0e86468032a05983874dbfa1fbe15673ac
DonJuan1232 <57268409+DonJuan1232@users.noreply.github.com>
2020-01-17 08:00:00 +0100

    added first coin mechanic

---
+++ sticky walls/Assets/Coin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Coin")
+        {
+            Debug.Log("coin");
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
